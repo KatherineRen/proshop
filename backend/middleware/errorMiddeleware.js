1db08706a34405ffb992fa9a8def27962a8b5378
Katherine1121 <807513497ren@gmail.com>
2023-10-16 16:01:12 -0400

    message

--- backend/middleware/errorMiddeleware.js
+++ backend/middleware/errorMiddeleware.js
@@ -1,19 +1,20 @@
 const notFound = (req, res, next) => {
-  const error = new Error(`Not Found -${req.orginialUrl}`)
-  res.status(404)
-  next(error)
-}
+  const error = new Error(`Not Found -${req.orginialUrl}`);
+  res.status(404);
+  next(error);
+};
 
 //default error handler
 const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
-  let message = err.message
+  console.log(err);
+  let message = err.message;
 
   res.status(statusCode).json({
     message,
     stack: process.env.Node_ENV === 'production' ? '🍽️' : err.stack,
-  })
-}
+  });
+};
 
-export { notFound, errorHandler }
+export { notFound, errorHandler };
